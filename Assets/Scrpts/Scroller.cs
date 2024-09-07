c6d670450fa6d8194f16b16b7c71623c86d117bd
Rishabh Dev Nayak <rdnayak26@gmail.com>
2024-09-07 15:01:06 +0530

    Scroller And Movement Logic Added(Testing Remaining)

---
+++ Assets/Scrpts/Scroller.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroller : MonoBehaviour
+{
+    [SerializeField] private RawImage rawImage;
+
+
+    private float x = 0, y = 0.1f;
+
+    public static Scroller Instance;
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        }
+    }
+
+    void scrollingLogic(Vector2 uvpos) {
+        rawImage.uvRect = new Rect(rawImage.uvRect.position + uvpos * Time.deltaTime, rawImage.uvRect.size);
+    }
+}
+    
+
